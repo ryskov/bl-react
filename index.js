549865f0e957d8de6373bfec315a985b154569a1
Ryskov <jrl@krugercorp.dk>
2016-10-01 22:39:01 +0200

    webpack and demo

---
+++ index.js
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom';
+
+import BL from './src/BL';
+
+window.onload = function () { ReactDOM.render(<BL.Demo/>, document.getElementById('root')) }
--- src/BL.js
+++ src/BL.js
@@ -1,5 +1,6 @@
 import _ from 'underscore';
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Panel from './components/Panel';
 import TextInput from './components/TextInput';
@@ -8,10 +9,19 @@ import HTTPHelper from './utils/HTTPHelper';
 window._ = _;
 window.React = React;
 
+const Demo = () => {
+    return (
+        <div>
+            <Panel width="300px">Hello!</Panel>
+        </div>
+    );
+};
+
 const BL = {
     // utils
     http: HTTPHelper,
-
+    
+    Demo,
     // components
     Panel,
     TextInput
