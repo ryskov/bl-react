a5d281cacc9e7b8a03c6bd7e086106b5d30a3f0e
Jeppe Larsen <jrl@patientsky.com>
2018-02-06 12:51:23 +0100

    now supports update of TextInput props

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -78,6 +78,12 @@ export default class TextInput extends Component {
         }
     }
 
+    componentWillReceiveProps(newProps) {
+        if (newProps.value) {
+            this.value = newProps.value;
+        }
+    }
+
     _renderInput() {
         const 
         {
